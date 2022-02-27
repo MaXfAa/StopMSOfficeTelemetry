a5ba108ac71d58d392a944e895bb5aa2a2dc8c14
Max <71935156+MaXfAa@users.noreply.github.com>
2022-02-27 21:40:30 +0100

    Update silent-worker.ps1

--- silent-worker.ps1
+++ silent-worker.ps1
@@ -2,7 +2,13 @@ New-Item -Path "HKCU:\Software\Policies\Microsoft" -Name office
 New-Item -Path "HKCU:\Software\Policies\Microsoft\office" -Name common
 New-Item -Path "HKCU:\Software\Policies\Microsoft\office\common" -Name clienttelemetry
 New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\office\common\clienttelemetry" -Name "DisableTelemetry" -Value "1" -PropertyType "DWord"
+New-Item -Path "HKCU:\Software\Policies\Microsoft\office" -Name 16.0
+New-Item -Path "HKCU:\Software\Policies\Microsoft\office\16.0" -Name common
+New-Item -Path "HKCU:\Software\Policies\Microsoft\office\16.0\common" -Name clienttelemetry
+New-ItemProperty -Path "HKCU:\Software\Policies\Microsoft\office\16.0\common\clienttelemetry" -Name "DisableTelemetry" -Value "1" -PropertyType "DWord"
+
 
 New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\common" -Name "sendcustomerdata" -Value "0" -PropertyType "DWord"
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\office\16.0\common" -Name "sendcustomerdata" -Value "0" -PropertyType "DWord"
 
 Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n0.0.0.0`thubblecontent.osi.office.net/contentsvc/api/telemetry" -Force
